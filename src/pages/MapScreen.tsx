3508a9f954f5a0767683942e6b024a9df76ec4fe
Wonchang0314 <andrew0314@naver.com>
2024-04-29 02:26:50 +0900

    FEAT: 맵스크린 퍼블리싱

--- src/pages/MapScreen.tsx
+++ src/pages/MapScreen.tsx
@@ -1,7 +1,8 @@
-import React from "react";
 import FlexBox from "../layout/FlexBox";
-import GridBox from "../layout/GridBox";
 import { useNavigate } from "react-router-dom";
+import Sider from "antd/es/layout/Sider";
+import { SearchOutlined } from "@ant-design/icons";
+import { Layout, Button, Input } from "antd";
 
 export default function MapScreen() {
   const navigate = useNavigate();
@@ -11,75 +12,92 @@ export default function MapScreen() {
   };
 
   return (
-    <div className="h-full">
-      <header className="h-16">
-        <nav>
-          <img src="/LOGO.png" alt="team logo" className="w-40 ml-8 mt-4" />
-        </nav>
-      </header>
-      
-    {/* 중간 부분 */}
-    <div className="bg-gray-100 h-screen px-0 mt-0">
-      <FlexBox className="h-full">
-        {/* 첫 번째 섹션 */}
-        <FlexBox direction="col" className="w-1/4 bg-gray-170 p-4 h-full"> 
-          {/* 첫 번째 행 */}
-          <FlexBox direction="col" className="w-full bg-white p-4 h-2/7"> 
-            <div className="flex items-center rounded-lg w-full border border-2 border-blue-500 p-2">
-              <input type="text" placeholder="ex)위치" className="outline-none flex-grow" />
-              <span role="img" aria-label="search" className="text-blue-500 cursor-pointer">🔍</span>
-            </div>
-            <FlexBox className="space-x-4 mt-4">
-              <button className="rounded-full bg-white border border-gray-400 py-2 px-4 text-xs hover:bg-gray-300 focus:outline-none">전체</button>
-              <button className="rounded-full bg-white border border-gray-400 py-2 px-4 text-xs hover:bg-gray-300 focus:outline-none">
-                <span role="img" aria-label="trash" className="text-gray-600">🗑️</span>
-                쓰레기
-                </button>
-              <button className="rounded-full bg-white border border-gray-400 py-2 px-4 text-xs hover:bg-gray-300 focus:outline-none">
-                <span role="img" aria-label="clothing" className="text-gray-600">👕</span>
-                의료수거함
-                </button>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider theme="light" className="shadow-md" width={288}>
+        <div className="flex flex-col items-center p-4 w-72">
+          <img src="/LOGO.png" alt="team logo" className="w-40 my-4" />
+          <div className="w-full relative">
+            <Input
+              type="text"
+              placeholder="ex) 서울특별시 강남구"
+              className="p-3 pl-3 pr-10 rounded-lg border border-2 border-blue-500"
+            />
+            <Button
+              icon={<SearchOutlined />}
+              className="absolute right-2 top-1/2 transform -translate-y-1/2"
+              style={{
+                border: "none",
+                background: "transparent",
+                boxShadow: "none",
+              }}
+            />
+          </div>
+          <FlexBox direction="row" className="mt-4 justify-between w-full">
+            <button className="rounded-full bg-white border border-gray-400 py-2 px-2 hover:bg-gray-300 focus:outline-none">
+              전체
+            </button>
+            <button className="rounded-full bg-white border border-gray-400 py-2 px-2 hover:bg-gray-300 focus:outline-none">
+              <img
+                src="/trash.svg.png"
+                alt="Trash"
+                className="inline-block mr-2 w-5 h-5"
+              />
+              휴지통
+            </button>
+            <button className="rounded-full bg-white border border-gray-400 py-2 px-2 hover:bg-gray-300 focus:outline-none">
+              <img
+                src="/icon.png"
+                alt="Trash"
+                className="inline-block mr-2 w-5 h-5"
+              />
+              의류수거함
+            </button>
+          </FlexBox>
+          <FlexBox direction="row" className="mt-4 mb-4">
+            <button className="mr-4 rounded-full bg-white border border-gray-400 py-2 px-2 hover:bg-gray-300 focus:outline-none">
+              <img
+                src="/Group.png"
+                alt="Trash"
+                className="inline-block mr-2 w-5 h-5"
+              />
+              폐의약품
+            </button>
+            <button className="rounded-full bg-white border border-gray-400 py-2 px-2 hover:bg-gray-300 focus:outline-none">
+              <img
+                src="/icon.png"
+                alt="Trash"
+                className="inline-block mr-2 w-5 h-5"
+              />
+              폐건전지
+            </button>
+          </FlexBox>
+          <div className="w-72 border-t-4 border-gray-100 my-2"></div>
+
+          <FlexBox direction="row" className="w-full justify-between">
+            <FlexBox direction="col">
+              <h2 className="mr-12 mb-1 text-l font-bold">위치정보</h2>
+              <p>서울특별시 동작구</p>
             </FlexBox>
-            
+            <button className="ml-8 border border-gray-400 py-2 px-2 rounded-full hover:bg-gray-100 focus:outline-none">
+              위치변경
+            </button>
           </FlexBox>
-                   
-          {/* 두 번째 행 */}
-          <FlexBox direction="col" className="w-full bg-white p-4 mt-3 h-1/7 items-start"> 
-            <h2 className="text-l font-bold mb-4 text-left">위치정보</h2>
-            <p className="text-left">서울특별시 동작구</p>
-            <button className="border border-gray-400 py-2 px-4 rounded-full hover:bg-gray-100 focus:outline-none ml-auto">위치변경</button>
-          </FlexBox>
-
-
-        
-          {/* 세 번째 행 */}
-          <FlexBox direction="col" className="w-full bg-white p-4 mt-3 h-full h-4/7 items-start"> 
-            <h2 className="text-l font-bold mb-4 text-left">주변 수거함</h2>
-            <img src="/search.png" alt="search img" className="w-24 mb-16" />
-            <p className="text-xs text-gray-400 text-center">검색결과가 없어요</p>
-            <p className="text-xs text-gray-400 text-center justify-center">다른 위치를 찾아보실래요?</p>
-          </FlexBox>
-      
-        </FlexBox>
-        
-        {/* 두 번째 섹션 */}
-        <FlexBox direction="col" className="w-3/4 bg-gray-300 p-4 h-full"> 
-          <h2 className="text-xl font-bold mb-4">지도 부분</h2>
-        </FlexBox>
-      </FlexBox>
-    </div>
-
-      <div className="bg-gray-100 px-24">
-        <FlexBox className="mt-64">
-          <img src="/LOGO.png" alt="team logo" className="w-24 mb-16" />
-          <FlexBox direction="col" className="mt-8">
-            <p className="text-sm mb-2 mr-2">TEAM NAME: Binfinder</p>
-            <p className="text-sm mb-16 ml-16 text-gray-400">
-              Title email: https://data.seoul.go.kr
+          <div className="w-72 border-t-4 border-gray-100 my-2"></div>
+          <div className="w-full">
+            <h2 className="text-l font-bold text-left">주변 수거함</h2>
+          </div>
+          <FlexBox direction="col" className="w-full mt-12">
+            <img src="/not-found.png" alt="search img" className="w-24 mb-8" />
+            <p className="text-xs text-gray-400 text-center">
+              검색결과가 없어요
+            </p>
+            <p className="text-xs text-gray-400 text-center justify-center">
+              다른 위치를 찾아보실래요?
             </p>
           </FlexBox>
-        </FlexBox>
-      </div>
-    </div>
+        </div>
+      </Sider>
+      {/* 나머지 레이아웃 컴포넌트 */}
+    </Layout>
   );
 }
