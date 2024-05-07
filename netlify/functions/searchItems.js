fe09cbd0eaac92236b46b69e2c29d54502134035
Wonchang0314 <andrew0314@naver.com>
2024-05-08 06:08:40 +0900

    FIX: 404에러 테스트

--- netlify/functions/searchItems.js
+++ netlify/functions/searchItems.js
@@ -2,10 +2,8 @@ import axios from "axios";
 
 exports.handler = async function (event, context) {
   const { keyWord, page, types } = event.queryStringParameters;
-  const typeQuery = types ? `&type=${types}` : "";
-  const url = `http://3.39.190.141:8080/search?query=${encodeURIComponent(
-    keyWord
-  )}${typeQuery}&page=${page}&size=20`;
+  const typeQuery = types.length ? `&type=${types.join(",")}` : "";
+  const url = `http://3.39.190.141:8080/search?query=${keyWord}${typeQuery}&page=${page}&size=20`;
 
   try {
     const response = await axios.get(url);
